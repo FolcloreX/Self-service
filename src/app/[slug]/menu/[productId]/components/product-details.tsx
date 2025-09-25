7ec26f453efd58d65ea6f90e0c483eb3f4a7134a
Folclore <chessmota@hotmail.com>
2025-09-25 19:27:34 -0300

    feat: add scroll to product details page

--- src/app/[slug]/menu/[productId]/components/product-details.tsx
+++ src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { formatCurrency } from "@/helpers/format-currency";
 import { Prisma } from "@prisma/client"
+import { ScrollArea } from "@radix-ui/react-scroll-area";
 import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import Image from "next/image"
 import { useState } from "react";
@@ -36,66 +37,75 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
     
 
     return (
-        <div className="relative z-50 mt-[-1.5rem] flex flex-auto flex-col rounded-t-3xl p-5">
-           <div className="flex-auto">
-             {/* RESTAURANTE */}
-            <div className="flex items-center gap-1">
-                <Image 
-                    src={product.restaurant.avatarImageUrl}
-                    alt={product.restaurant.name}
-                    width={16}
-                    height={16}
-                    className="rounded-full"
-                />
-                <p className="text-xs text-muted-foreground">
-                    {product.restaurant.name}
-                </p>
-            </div>
-
-            {/* NOME DO PRODUTO */}
-            <h2 className="mt-1 text-xl font-semibold">{product.name}</h2>
-            
-            {/* PREÇO E QUANTIDADE */}
-            <div className="flex items-center justify-between">
-                <h3 className="text-xl font-semibold">
-                    {formatCurrency(product.price)}
-                </h3>                 
-                <div className="flex items-center gap-3 text-center">
-                    <Button 
-                        variant="outline"
-                        className="h-8 w-8 rounded-xl"
-                        onClick={handleDecreaseQuantity}
-                    >
-                        <ChevronLeftIcon />    
-                    </Button> 
-                    <p className="w-4">{quantity}</p>
-                    <Button 
-                        variant="destructive"
-                        className="h-8 w-8 rounded-xl"
-                        onClick={handleIncreaseQuantity}
-                    >
-                        <ChevronRightIcon />    
-                    </Button>   
+        <div className="relative z-50 mt-[-1.5rem] flex flex-auto flex-col rounded-t-3xl p-5 overflow-hidden">
+           <div className="flex-auto overflow-hidden">
+                {/* RESTAURANTE */}
+                <div className="flex items-center gap-1">
+                    <Image 
+                        src={product.restaurant.avatarImageUrl}
+                        alt={product.restaurant.name}
+                        width={16}
+                        height={16}
+                        className="rounded-full"
+                    />
+                    <p className="text-xs text-muted-foreground">
+                        {product.restaurant.name}
+                    </p>
                 </div>
-            </div>
 
-            {/* SOBRE */}
-            <div className="mt-6 space-y-3">
-                <h4 className="font-semibold">Sobre</h4>
-                <p className="text-sn text-muted-foreground">{product.description}</p>
-            </div>
+                {/* NOME DO PRODUTO */}
+                <h2 className="mt-1 text-xl font-semibold">{product.name}</h2>
+                
+                {/* PREÇO E QUANTIDADE */}
+                <div className="flex items-center justify-between mt-3">
+                    <h3 className="text-xl font-semibold">
+                        {formatCurrency(product.price)}
+                    </h3>                 
+                    <div className="flex items-center gap-3 text-center">
+                        <Button 
+                            variant="outline"
+                            className="h-8 w-8 rounded-xl"
+                            onClick={handleDecreaseQuantity}
+                        >
+                            <ChevronLeftIcon />    
+                        </Button> 
 
-            {/* INGREDIENTS */}
-            <div className="mt-6 space-y-3">
-                <div className="flex items-center gap-1.5">
-                    <ChefHatIcon size={18}/>
+                        <p className="w-4">{quantity}</p>
+
+                        <Button 
+                            variant="destructive"
+                            className="h-8 w-8 rounded-xl"
+                            onClick={handleIncreaseQuantity}
+                        >
+                            <ChevronRightIcon />    
+                        </Button>   
+                    </div>
                 </div>
-                <h4 className="font-semibold">Sobre</h4>
-                <p className="text-sn text-muted-foreground">{product.description}</p>
-            </div>
+
+                {/* SOBRE E INGREDIENTS*/}
+                <ScrollArea className="h-full">
+                    {/* SOBRE */}
+                    <div className="mt-6 space-y-3">
+                        <h4 className="font-semibold">Sobre</h4>
+                        <p className="text-sn text-muted-foreground">{product.description}</p>
+                    </div>
+
+                    {/* INGREDIENTS */}
+                    <div className="mt-6 space-y-3">
+                        <div className="flex items-center gap-1.5">
+                            <ChefHatIcon size={18}/>
+                            <h4 className="font-semibold">Ingredients</h4>
+                        </div>      
+                        <ul className="list-disc px-5 text-sn text-muted-foreground">
+                            {product.ingredients.map((ingredient) => (
+                                <li key={ingredient}>{ingredient}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </ScrollArea>
            </div>
 
-            <Button className="w-full rounded-full">Adicionar à sacola</Button>
+            <Button className="mt-6 w-full rounded-full">Adicionar à sacola</Button>
         </div>
     )
 }
