310ede7f6afe18ce6a279c90a00a916970c47649
Folclore <chessmota@hotmail.com>
2025-09-28 01:10:53 -0300

    feat: ensure cart product quantity can be decrease

--- src/app/[slug]/menu/context/cart.tsx
+++ src/app/[slug]/menu/context/cart.tsx
@@ -13,6 +13,7 @@ export interface ICartContext{
     products: CartProduct[],
     toggleCart: () => void;
     addProduct: (product: CartProduct) => void;
+    decreaseProductQuantity: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -20,6 +21,7 @@ export const CartContext = createContext<ICartContext>({
     products: [],
     toggleCart: () => {},
     addProduct: () => {},
+    decreaseProductQuantity: () => {},
 });
 
 export const CartProvider = ({ children }: { children: ReactNode }) => {
@@ -46,12 +48,28 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
                     return {
                         ...prevProduct,
                         quantity: prevProduct.quantity + product.quantity,
-                    }
+                    };
                 }
                 return prevProduct
-            })
-        })
-    }
+            });
+        });
+    };
+
+    const decreaseProductQuantity = (productId: string) => {
+        setProducts(prevProducts => {
+            return prevProducts.map(prevProduct => {
+                if (prevProduct.id != productId) {
+                    return prevProduct;    
+                }
+
+                if (prevProduct.quantity === 1) {
+                    return prevProduct;
+                }
+
+                return {...prevProduct, quantity: prevProduct.quantity - 1};
+            });
+        });
+    };
 
     return (
         <CartContext.Provider 
@@ -59,7 +77,8 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
                 isOpen,
                 products,
                 toggleCart,
-                addProduct
+                addProduct,
+                decreaseProductQuantity,
             }}
         >
             {children}
